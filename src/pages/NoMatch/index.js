8721dbbed957b602b87576cb9b736c462cb15170
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2021-03-03 14:26:15 -0800

    Testing path issue.

--- src/pages/NoMatch/index.js
+++ src/pages/NoMatch/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from '../../components/Header';
 import { Container, Row, Col } from "../../components/Grid";
 
 function NoMatch() {
     return(
+        <div>
+        <Header />
         <Container classes="mt-5">
             <Row classes="p-5">
                 <Col size="12" classes="text-center p-5 contentBox">
@@ -13,6 +16,7 @@ function NoMatch() {
                 </Col>
             </Row>
         </Container>
+        </div>
     )
 }
 
