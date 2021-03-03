2d09939084819fe41c18a1ef151642616d8ede51
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2021-03-02 18:28:54 -0800

    Update AboutMe page.

--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from '../../components/NavBar';
+import MyPic from '../../images/me-2.jpg'
 import { Container, Row, Col } from "../../components/Grid";
 import Footer from '../../components/Footer';
 
@@ -7,7 +8,7 @@ function AboutMe() {
     return (
         <div className="d-flex flex-column" style={{height: "100vh"}}>
             <NavBar />
-            <Container classes="mt-5">
+            <Container classes="mt-5 mb-5">
                 <Col size="lg-9 md-10 sm-12" classes="mt-3 contentBox">
                     <Row>
                         <Col size="12" classes="m-3">
@@ -17,6 +18,7 @@ function AboutMe() {
                     </Row>
                     <Row>
                         <Col size="12">
+                        <img className="my-3 mx-4 aboutMePic" src={MyPic} alt="Me" style={{width: "200px", borderRadius: "50%", float: "left"}}/>
                         <p className="m-3">My name is Christina, and I'm a web developer passionate in building and maintaining web applications using multiple web technologies.  With a background as a technology specialist for a large finance company, I have developed skills in customer service as well as: working well with a team, solving complex problems in a timely manner, and tackling challenges with an open-mind while being accountable for my responsibilities.  I was also recognized for identifying a workflow issue, and played a major role in developing a solution that significantly decreased our processing time, creating a more positive experience for my team, the company, and our clients. </p>
                         </Col>
                     </Row>
@@ -29,7 +31,7 @@ function AboutMe() {
                     </Row>
                     <Row classes="mb-3">
                         <Col size="12">
-                            <p className="m-3">Please use the navigation bar at the top to enjoy looking through some of my projects that I have already completed by navigating over to my Portfolio page!  Please also use my Contact Page to reach out to me!</p>
+                            <p className="m-3">Please use the navigation bar at the top to enjoy looking through some of my projects that I have already completed by navigating over to My Portfolio page!  Please also use my Contact Me page to reach out to me!</p>
                         </Col>
                     </Row>
                 </Col>
