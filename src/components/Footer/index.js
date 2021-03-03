81baddfde805d9cb27f46508d5a293352b710e24
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2021-03-02 18:21:00 -0800

    Add Footer. Update styles.

---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './styles.css';
+
+function Footer() {
+    const date = new Date();
+    const year = date.getFullYear();
+
+    return(
+        <footer className="mt-auto footerAll">
+            <div className="footerRow"></div>
+            <div className="container py-3">
+                <div className="row footerSpace">
+                    <div className="col">
+                        <span className="mx-auto">&copy; Copyright {year} &nbsp;&nbsp; <a className="footer-icon me-2" href="https://www.linkedin.com/in/christina2021/"><i className="fab fa-linkedin"></i></a><a className="ml-1 footer-icon" href="https://github.com/Christina2021"><i className="fab fa-github-square"></i></a></span>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -1,38 +1,40 @@
 import React from 'react';
 import NavBar from '../../components/NavBar';
 import { Container, Row, Col } from "../../components/Grid";
+import Footer from '../../components/Footer';
 
 function AboutMe() {
     return (
-        <div>
+        <div className="d-flex flex-column" style={{height: "100vh"}}>
             <NavBar />
-            <Container classes="contentBox">
-                <Col size="lg-9 md-10 sm-12" classes="mt-5">
+            <Container classes="mt-5">
+                <Col size="lg-9 md-10 sm-12" classes="mt-3 contentBox">
                     <Row>
-                        <Col size="" classes="m-3">
+                        <Col size="12" classes="m-3">
                             <h1>About Me</h1>
                             <hr />
                         </Col>
                     </Row>
                     <Row>
-                        <Col size="">
+                        <Col size="12">
                         <p className="m-3">My name is Christina, and I'm a web developer passionate in building and maintaining web applications using multiple web technologies.  With a background as a technology specialist for a large finance company, I have developed skills in customer service as well as: working well with a team, solving complex problems in a timely manner, and tackling challenges with an open-mind while being accountable for my responsibilities.  I was also recognized for identifying a workflow issue, and played a major role in developing a solution that significantly decreased our processing time, creating a more positive experience for my team, the company, and our clients. </p>
                         </Col>
                     </Row>
                     <Row>
-                        <Col size="">
+                        <Col size="12">
                             <p className="m-3">I have received a certificate in the Coding Bootcamp through the University of California, San Diego, and am knowledgeable and experienced with front-end and back-end application development technologies, which include:</p>
                             <p className="mx-3 text-center fst-italic fw-bold">HTML, CSS, Bootstrap, Foundation, JavaScript, jQuery, API-management, Node.js, Express, Express-Handlebars, EJS, MySQL, Sequelize, MongoDB, and React.</p>
                             <p className="m-3">I look forward to continuing learning and growing as a developer.</p>
                         </Col>
                     </Row>
                     <Row classes="mb-3">
-                        <Col size="">
+                        <Col size="12">
                             <p className="m-3">Please use the navigation bar at the top to enjoy looking through some of my projects that I have already completed by navigating over to my Portfolio page!  Please also use my Contact Page to reach out to me!</p>
                         </Col>
                     </Row>
                 </Col>
             </Container>
+            <Footer />
         </div>
     )
 }
